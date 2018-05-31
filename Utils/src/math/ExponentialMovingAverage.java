a972d75c924442595b319ef3a1b9b3ca30ae0fa7
adam <adamschunk@gmail.com>
2018-05-31 15:03:36 -0400

    added functionality for double array

--- Utils/src/math/ExponentialMovingAverage.java
+++ Utils/src/math/ExponentialMovingAverage.java
@@ -16,4 +16,16 @@ public class ExponentialMovingAverage {
         oldValue = newValue;
         return newValue;
     }
+    
+    public double[] average(double[] values) {
+    	double[] res = new double[values.length];
+    	if (oldValue == null) {
+            oldValue = values[0];
+            res[0] = oldValue;
+        }
+        for(int i = 1; i < values.length; i++) {
+        	res[i] = average(values[i]);
+        }
+        return res;
+    }
 }
